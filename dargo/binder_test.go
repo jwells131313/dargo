5e24e329fefe9a5483f52f7bbd83d5a74f97cfef
John Wells <john.wells@oracle.com>
2018-02-07 17:43:46 +0100

    Change to use -v on test for now

--- dargo/binder_test.go
+++ dargo/binder_test.go
@@ -3,19 +3,23 @@ package dargo
 import "testing"
 import "reflect"
 
+type shape interface {
+	
+}
+
 func TestBinder(t *testing.T) {
   desc := new(Descriptor)
   name := "Nick Foles"
   
-  Bind(desc, reflect.TypeOf(new(Shape)).Elem(), name, nil)
+  Bind(desc, reflect.TypeOf(new(shape)).Elem(), name, nil)
 }
 
 func TestPointerBinderFailure(t *testing.T) {
   desc := new(Descriptor)
   name := "Nick Foles"
   
-  error := Bind(desc, reflect.TypeOf(new(Shape)), name, nil)
+  error := Bind(desc, reflect.TypeOf(new(shape)), name, nil)
   if (error == nil) {
-  	t.Errorf("Should have been a failure because reflect.TypeOf(new(Shape)) is a pointer")
+  	t.Errorf("Should have been a failure because reflect.TypeOf(new(shape)) is a pointer")
   }
 }
